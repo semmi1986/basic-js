afb0cbc43de6a63631a6a2a73b4a86e59fd93287
Semen Samosiuk <samasiuk1986@gmail.com>
2022-10-13 17:31:56 +0300

    feat: add solution DNSstats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
+function getDNSStats(domains) {
+//   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  const domainsObj = {};
+
+  for (let i = 0; i < domains.length; i++) {
+	 let summAppDns = '';
+	 let appDns = domains[i].split('.').reverse();
+  //   console.log(appDns);
+
+	 for (let j = 0; j < appDns.length; j++) {
+	  // console.log(appDns[j]);
+		let simbolDns = '.' + appDns[j];
+		summAppDns += simbolDns;
+	  //  console.log(summAppDns);
+		
+
+	  domainsObj[summAppDns] ? domainsObj[summAppDns]++ : domainsObj[summAppDns] = 1;
+
+	  //  console.log(domainsObj[summAppDns]);
+	 }
+
+  }
+
+  // console.log(domainsObj);
+  return domainsObj;
+
 }
 
 module.exports = {
