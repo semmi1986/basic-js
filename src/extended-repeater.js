e892d63e0ec7e2e6727fecf539493f82ac819c16
Semen Samosiuk <samasiuk1986@gmail.com>
2022-10-14 16:11:07 +0300

    feat: add solution extendsRepeat

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+	//   throw new NotImplementedError('Not implemented');
+	// remove line with error and write your code here
+
+	let newArrStr = [];
+	let addArr = [];
+	// string add
+	let repeatStr = (options.hasOwnProperty('repeatTimes')) ? options.repeatTimes : 1;
+	let addStr = (options.hasOwnProperty('addition')) ? `${options.addition}` : '';
+	let separ = (options.hasOwnProperty('separator')) ? options.separator : '+';
+	// portion strstring
+	let addRepeatStr = (options.hasOwnProperty('additionRepeatTimes')) ? options.additionRepeatTimes : 1;
+	let addSepar = (options.hasOwnProperty('additionSeparator')) ? options.additionSeparator : '|';
+
+	//new array depending of value repeatTimes and add addition to str
+	//addRepeatStr=2 => ['addStr(addSepar)', 'addStraddStr(addSepar)']
+	for (let i = 1; i <= addRepeatStr; i++) {
+		addArr.push(addStr);
+	}
+	// repeatStr=2 => str.addStr.str.addStr(addSepar)
+
+	for (let i = 1; i <= repeatStr; i++) {
+		newArrStr.push(str + addArr.join(`${addSepar}`))
+	}
+	return newArrStr.join(`${separ}`)
 }
 
 module.exports = {
-  repeater
+	repeater
 };
